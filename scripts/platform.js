e8517266c8979ba84830dd045ca3fdfd01699024
Dan Daly <bassoonmandan187@gmail.com>
2014-02-26 13:29:10 -0500

    Added folder for scripts as well as the functional platform script.

---
+++ scripts/platform.js
@@ -0,0 +1,23 @@
+// Use this for initialization
+function Start () {
+}
+
+// Update is called once per frame
+function Update () {
+}
+
+function OnTriggerEnter (jumper: Collider) {
+//make the parent platform ignore the jumper
+	var platform = transform.parent;
+
+    Physics.IgnoreCollision(jumper.GetComponent(CharacterController), platform.GetComponent(BoxCollider));
+
+}
+
+function OnTriggerExit (jumper: Collider) {
+//re-enable collision between jumper and parent platform
+    var platform = transform.parent;
+
+    Physics.IgnoreCollision(jumper.GetComponent(CharacterController), platform.GetComponent(BoxCollider), false);
+
+}
