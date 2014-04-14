36bec91b66b5181dc8115ddcaff5c6d623169795
jackjamieson <jamieson.jack@gmail.com>
2014-04-13 22:22:54 -0400

    moved the music to the main camera to avoid distortion, added basic main menu scene

---
+++ punchout/Assets/mainMenu.js
@@ -0,0 +1,31 @@
+﻿#pragma strict
+
+var isQuit=false;
+
+function OnMouseEnter(){
+//change text color
+renderer.material.color=Color.black;
+}
+
+function OnMouseExit(){
+//change text color
+renderer.material.color=Color.red;
+}
+
+function OnMouseUp(){
+//is this quit
+if (isQuit==true) {
+//quit the game
+Application.Quit();
+}
+else {
+//load level
+Application.LoadLevel(1);
+}
+}
+
+function Update(){
+//quit game if escape key is pressed
+if (Input.GetKey(KeyCode.Escape)) { Application.Quit();
+}
+}
