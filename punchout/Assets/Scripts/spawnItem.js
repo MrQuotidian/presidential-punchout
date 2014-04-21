2ad38af40d5ca0fdcfe19123774c29a76d9af459
Jack Jamieson <jamieson.jack@gmail.com>
2014-04-21 19:04:17 -0400

    added item dropper script and new prefab for items

--- punchout/Assets/Scripts/spawnItem.js
+++ punchout/Assets/Scripts/spawnItem.js
@@ -1,6 +1,10 @@
 ﻿#pragma strict
 
 var init = false;
+var goLeft = false;
+var goRight = false;
+var item1 : GameObject;
+var itemClone : Rigidbody;
 
 function Start () {
 	
@@ -8,17 +12,47 @@ function Start () {
 
 function Update () {
 	
+	var makeItem = Random.Range(0, 1000);
+	if(makeItem == 1)
+	{
+		dropItem();
+	}
+	
 	if(!init)
 		transform.Translate(Vector3(5,0,0) * Time.deltaTime);
-
 	
-	if(transform.position.x <= -20)
-		transform.Translate(Vector3(5, 0, 0) * Time.deltaTime);
-	else if(transform.position.x > 0 && transform.position.x <= 20)
+	if(transform.position.x < 0 && transform.position.x <= -20)
+	{
+		//init = true;
+		goLeft = false;
+		goRight = true;
+	}
+	if(transform.position.x > 0 && transform.position.x >= 20)
 	{
 		init = true;
+		goLeft = true;
+		goRight = false;
+	}
+	
+	if(goRight)
+	{
+		transform.Translate(Vector3(5, 0, 0) * Time.deltaTime);
+
+	}
+	
+	if(goLeft)
+	{
 		transform.Translate(Vector3(-5, 0, 0) * Time.deltaTime);
 	}
 	
+
+}
+
+function dropItem()
+{
+	//yield WaitForSeconds(10);
+	Debug.Log("Drop Item");
+	itemClone = Instantiate(item1, transform.position, Quaternion.identity).rigidbody;
+	itemClone.velocity = -transform.up * Time.deltaTime * 2;
 
 }
